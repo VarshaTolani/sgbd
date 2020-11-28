d70abbb4106be660d8524af10bdaccde5a0d4b09
PauCasademont <61241126+PauCasademont@users.noreply.github.com>
2020-11-28 15:28:48 +0100

    Connexio amb ES

--- src/App.js
+++ src/App.js
@@ -1,14 +1,41 @@
-import React from 'react';
-import Mapa from './Map'
+import React, { useState, useEffect } from 'react';
+import Map from './Map'
+import axios from 'axios'
 
-function App(){
 
-  return(
-    <div className="App">
-      <Mapa />
-    </div>
-  );
 
+function App() {
+  
+  const url = "http://localhost:9200/restaurants/_search"
+
+  const query = {
+                  "_source": ["nom","localitzacio"],
+                  "size": 10000,
+                  "query": {
+                    "bool": {
+                      "must": {
+                        "match_all": {}
+                      }
+                    }
+                  }
+                };
+
+
+  const [positions, setPositions] = useState(null)
+
+  useEffect(() => {
+    let cancel
+    axios.post(url, query, {
+      cancelToken: new axios.CancelToken(c => cancel = c)
+    }).then(res => {
+      setPositions(res.data.hits.hits.map(res => res._source.localitzacio))
+    }).catch(error => console.log('ERROR: ', error.response))
+    return () => cancel() 
+  }, [url])
+
+  return (
+    <Map  positions = {positions}/>
+  )
 }
 
 export default App;
