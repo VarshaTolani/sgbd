e8660cebadf06800d723bc7d565ecb74d9aa8a09
PauCasademont <61241126+PauCasademont@users.noreply.github.com>
2020-11-28 15:27:34 +0100

    Markers
    Mapa passa a ser una funcio export deafult.
    Es mostren tots els punts que rep per parametre

--- src/Map.js
+++ src/Map.js
@@ -6,64 +6,63 @@ import Vector from 'ol/layer/Vector'
 import Feature from 'ol/Feature'
 import Point from 'ol/geom/Point'
 import { fromLonLat } from 'ol/proj.js';
-import React, {Component} from 'react';
+import React, {Component, useEffect, useRef} from 'react';
 import {Circle, Fill, Style} from 'ol/style';
 import Icon from 'ol/style/Icon';
 import {transform} from 'ol/proj';
 import VectorSource from 'ol/source/Vector';
 import { Vector as VectorLayer} from 'ol/layer';
 
-export default class Mapa extends React.Component{
 
-  
 
-  componentDidMount() {
-    
-    this.chicago = new Feature({
-      geometry: new Point(fromLonLat([-87.623177, 41.881832]))
+function initialize_map(positions){
+  console.log('Positions: ', positions)
+  if (positions){
+    let geo_points = positions.map(p => 
+      new Feature({
+        geometry: new Point(fromLonLat(p.split`,`.map(x => +x)))
+      }) )
+
+    let vectorSource = new VectorSource({
+      features: geo_points
     });
 
-    this.london = new Feature({
-      geometry: new Point(fromLonLat([-0.12755, 51.507222]))
-    });
-
-    this.madrid = new Feature({
-      geometry: new Point(fromLonLat([-3.683333, 40.4]))
-    });
-
-    this.vectorSource = new VectorSource({
-      features: [this.chicago, this.madrid, this.london]
-    });
-
-    this.vectorLayer = new VectorLayer({
-      source: this.vectorSource,
-	  style: new Style({
+    let vectorLayer = new VectorLayer({
+      source: vectorSource,
+      style: new Style({
         image: new Circle({
           radius: 6,
           fill: new Fill({color: 'red'}),
         }),
       }),
     });
-
-    this.map = new Map({
+    
+    return new Map({
       target: 'mapContainer',
       layers: [ new TileLayer({
-       source: new OSM()
-      }), this.vectorLayer ],
+          source: new OSM() }),
+          vectorLayer ],
       view: new View({
         center: fromLonLat([2.896372, 44.60240]),
         zoom: 3
-      }),
-    });
-  
-  
+      })
+    }) 
   }
+    
+}
 
-  render(){
-    console.log('-> render App')
-    return(
-      <div id="mapContainer" ref="mapContainer" style={{width: '80vw', height: '80vh'}}> </div>
-    );
-  }
 
+
+
+export default function Mapa( { positions }) {
+
+  const map = useRef(null)
+
+  useEffect(() => {
+    map.current = initialize_map(positions) 
+  })
+  
+  return(
+    <div id="mapContainer" ref={map.current} style={{width: '80vw', height: '80vh'}}> </div>
+  )
 }
