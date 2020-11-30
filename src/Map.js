122c098d9c689f3ec33ae8967dfa302c02c22e71
varsha <varshatolani98@gmail.com>
2020-11-30 20:42:47 +0100

    Pull local

--- src/Map.js
+++ src/Map.js
@@ -1,69 +1,69 @@
-
 import 'ol/ol.css';
 import {Map, View} from 'ol';
-import TileLayer from 'ol/layer/Tile';
+import {Tile as TileLayer, Vector as VectorLayer} from 'ol/layer';
+import VectorSource from 'ol/source/Vector';
 import OSM from 'ol/source/OSM';
-import React, {Component} from 'react';
+import Vector from 'ol/layer/Vector'
+import Feature from 'ol/Feature'
+import Point from 'ol/geom/Point'
+import { fromLonLat } from 'ol/proj.js';
+import React, {Component, useEffect, useRef} from 'react';
+import {Circle, Fill, Style} from 'ol/style';
+import Icon from 'ol/style/Icon';
+import {transform} from 'ol/proj';
+import VectorSource from 'ol/source/Vector';
+import { Vector as VectorLayer} from 'ol/layer';
 
-export default class Mapa extends React.Component{
 
-  constructor(props) {
-        super(props);
 
-        this.view = new View({
-            center: [0,0],
-            zoom: 2
-        });
-    }
+function initialize_map(positions){
+  console.log('Positions: ', positions)
+  if (positions){
+    let geo_points = positions.map(p =>
+      new Feature({
+        geometry: new Point(fromLonLat(p.split`,`.map(x => +x)))
+      }) )
 
-  componentDidMount() {
-      this.map =  new Map({
-          view: this.view,
-          layers: [new TileLayer({ source: new OSM() })],
-          target: this.refs.mapContainer
-      });
-  }
+    let vectorSource = new VectorSource({
+      features: geo_points
+    });
 
-  render(){
-    console.log('-> render App')
-    return(
-      <div id="mapContainer" ref="mapContainer" style={{width: '80vw', height: '80vh'}}> </div>
-    );
+    let vectorLayer = new VectorLayer({
+      source: vectorSource,
+      style: new Style({
+        image: new Circle({
+          radius: 6,
+          fill: new Fill({color: 'red'}),
+        }),
+      }),
+    });
+
+    return new Map({
+      target: 'mapContainer',
+      layers: [ new TileLayer({
+          source: new OSM() }),
+          vectorLayer ],
+      view: new View({
+        center: fromLonLat([2.896372, 44.60240]),
+        zoom: 3
+      })
+    })
   }
 
 }
 
 
-/*import { GoogleMap,
-        withScriptjs,
-        withGoogleMap,
-        Marker
-     } from "react-google-maps"
 
-const Politecnica_UDG_position = { lat: 41.964162, lng: 2.830088 };
 
-function CreateMap() {
-    return (
-        <GoogleMap
-          defaultZoom={10}
-          defaultCenter={{lat: 41.981651, lng: 2.823610}}
-        >
-            <Marker position={Politecnica_UDG_position}/>
-        </GoogleMap>
-    );
+export default function Mapa( { positions }) {
+
+  const map = useRef(null)
+
+  useEffect(() => {
+    map.current = initialize_map(positions)
+  })
+
+  return(
+    <div id="mapContainer" ref={map.current} style={{width: '80vw', height: '80vh'}}> </div>
+  )
 }
-
-const WrappedMap = withScriptjs(withGoogleMap(CreateMap));
-
-export default function Map(){
-    return(
-      <div style={{width: '70vw', height: '70vh'}}>
-        <WrappedMap
-            googleMapURL= {"https://maps.googleapis.com/maps/api/js?key=AIzaSyDzw2uiT3nAzHYONGS9fEzOi7tOjhQ0VsY&v=3.exp&libraries=geometry,drawing,places"}
-            loadingElement={ <div style={{ height: "100%" }} /> }
-            containerElement={ <div style={{ height: "100%" }} /> }
-            mapElement={ <div style={{ height: "100%" }} /> }
-        />
-      </div>
-    )
-}*/
