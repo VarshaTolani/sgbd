b73490e4852d516746ff8a94ca9b36e890d7e2a5
Jordi Gomara Cruz <62349008+u1956236@users.noreply.github.com>
2020-12-05 16:15:41 +0100

    Add files via upload

--- src/Filters.js
+++ src/Filters.js
@@ -4,9 +4,18 @@ export default function Filters( { get_new_query }){
     return(
         <>
 
-            <p> </p>        
-            <label>Distància Maxima</label>
-            <p> </p>   
+<table>
+  <tr>
+    <th>Distància</th>
+	<th></th>
+    <th>Tipus</th>
+	<th></th>
+    <th>Preu</th>
+	<th></th>
+    <th>Valoracio</th>
+  </tr>
+  <tr>
+    <td>
             <select id="distancia">
             <option selected value="0"> </option>
                 <option value="1km">1km</option>
@@ -14,42 +23,40 @@ export default function Filters( { get_new_query }){
                 <option value="100km">100km</option>
                 <option value="10000km">10000km</option>
             </select>            
-            <p> </p>
-
-            <p> </p>        
-            <label>Tipus</label>
-            <p> </p>   
-            <select id="tipus">
+	</td>
+	<td></td>
+    <td>
+			<select id="tipus">
             <option selected value="sense tipus"> </option>
                 <option value="Italiana">Italiana</option>
                 <option value="Japonesa">Japonesa</option>
                 <option value="Mexicana">Mexicana</option>
-            </select>            
-            <p> </p>
-            
-            <label>Escull el preu</label>
-            <p> </p>
-            <select id="preu">
+            </select>
+	</td>
+    <td></td>
+	<td>
+		<select id="preu">
             <option selected value="0"> </option>
                 <option value="20">20€</option> 
                 <option value="40">40€</option> 
                 <option value="70">70€</option> 
                 <option value="100">100€</option> 
             </select>
-            
-            <p> </p>
-            
-            <label for="Valoracio">Escull la valoració</label>
-            <p> </p>
-            <select id="valoracio" name="Valoracio">
+	</td>
+	<td></td>
+	<td>
+			<select id="valoracio" name="Valoracio">
             <option selected value="0"> </option> 
                 <option value="1">1 estrella</option> 
                 <option value="2">2 estrelles</option> 
                 <option value="3">3 estrelles</option> 
                 <option value="4">4 estrelles</option> 
                 <option value="5">5 estrelles</option> 
-            </select>            
-            <p> </p>
+            </select>
+	</td>
+  </tr>
+</table>
+                      
             <div>
                 {get_new_query && <button onClick={get_new_query}>Buscar</button>}
             </div>
